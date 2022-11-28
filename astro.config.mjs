08affcb4ea6485076b2bb7cbc85086400821e092
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-28 06:22:05 -0500

    switch from astro-imagetools to @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { astroImageTools } from 'astro-imagetools';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import image from '@astrojs/image';
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), astroImageTools],
+	integrations: [
+		tailwind(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
 });
