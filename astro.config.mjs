c9e9e54818b3775220eec9d0105ada74ff66906d
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-28 04:25:30 -0500

    astro-imagetools to optimize hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+import { astroImageTools } from 'astro-imagetools';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	integrations: [tailwind(), astroImageTools],
 });
