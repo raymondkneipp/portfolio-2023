63620524b9111ed71a6dffb826cca4f0e142323a
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-01 04:35:41 -0500

    add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://raymondkneippbeta.vercel.app/',
-	integrations: [
-		robotsTxt(),
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-		mdx(),
-	],
+  site: 'https://raymondkneippbeta.vercel.app/',
+  integrations: [robotsTxt(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx(), sitemap()]
 });
